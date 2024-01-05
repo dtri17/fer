8fdaabcf7b01f08fd062467e99d25d94568636f1
dtri17 <trindde170428@fpt.edu.vn>
2024-01-05 14:17:09 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// console.log("Đây là index.js");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
